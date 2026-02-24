226dbf111a9fba4b227c300b415dbfb6632136df
Omar3597 <24003289@o6u.edu.eg>
2026-02-24 19:39:09 +0200

    feat(auth): change reset and verify email routes to GET method

--- src/modules/user/auth/auth.routes.ts
+++ src/modules/user/auth/auth.routes.ts
@@ -12,8 +12,8 @@ router.post("/register", authController.createUser);
 router.post("/login", authController.login);
 router.post("/refresh", authController.getrefreshToken);
 router.post("/forgot-password", authController.forgotPassword);
-router.post("/reset-password/:token", authController.resetPassword);
-router.post("/verify-email/:token", authController.verifyEmail);
+router.get("/reset-password/:token", authController.resetPassword);
+router.get("/verify-email/:token", authController.verifyEmail);
 router.post("/logout", protect, authController.logout);
 
 export default router;

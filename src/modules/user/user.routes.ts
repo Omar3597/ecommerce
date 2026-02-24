6b344dff30a691d8dd8a5984ad7fc94371a92a31
Omar3597 <24003289@o6u.edu.eg>
2026-02-24 19:41:29 +0200

    refactor(user): restructure user routes for improved organization and clarity

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,22 +1,25 @@
 import express from "express";
 import authRouter from "./auth/auth.routes";
+import addressRouter from "./../address/address.routes";
 import { protect } from "../../common/middlewares/protect";
 import { UserService } from "./user.service";
 import { UserController } from "./user.controller";
 
 const router = express.Router();
-
 const userService = new UserService();
 const userController = new UserController(userService);
 
 router.use("/auth", authRouter);
 
-router.use(protect);
+const meRouter = express.Router();
 
-router.get("/me", userController.getMe);
-router.patch("/me/update-profile", userController.updateProfile);
-router.patch("/me/update-password", userController.updatePassword);
-router.post("/me/email-change", userController.requestEmailChange);
-router.post("/me/email-change/verify/:token", userController.verifyEmailChange);
+meRouter.get("/", userController.getMe);
+meRouter.patch("/profile", userController.updateProfile);
+meRouter.patch("/password", userController.updatePassword);
+meRouter.post("/email-change", userController.requestEmailChange);
+meRouter.get("/email-change/verify/:token", userController.verifyEmailChange);
+meRouter.use("/address", addressRouter);
+
+router.use("/me", protect, meRouter);
 
 export default router;

107ee93284c6b8d39d20976f66e7dcd921bc7685
Omar Elgouhary <24003289@o6u.edu.eg>
2026-04-04 17:12:29 +0200

    feat(shipping): add shipping simulator to automate order status updates

--- src/common/jobs/cron.ts
+++ src/common/jobs/cron.ts
@@ -2,10 +2,12 @@ import { cleanupExpiredOrders } from "./handlers/cleanupOrders";
 import { cleanupExpiredTokens } from "./handlers/cleanupTokens";
 import { cleanupStaleCarts } from "./handlers/cleanupCarts";
 import { cleanupUnverifiedUsers } from "./handlers/cleanupUsers";
+import { runShippingSimulator } from "./handlers/shippingSimulator";
 
 export const initCronJobs = () => {
   cleanupExpiredOrders();
   cleanupExpiredTokens();
   cleanupStaleCarts();
   cleanupUnverifiedUsers();
+  runShippingSimulator();
 };
